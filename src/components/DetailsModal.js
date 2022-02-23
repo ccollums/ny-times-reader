b03254307729427db19e40d358f82f93203c6ffe
Carly Collums <carlycollums@gmail.com>
2022-02-23 16:36:25 -0700

    Add article details to modal along with ability to close modal

--- src/components/DetailsModal.js
+++ src/components/DetailsModal.js
@@ -1,11 +1,17 @@
 import '../css/DetailsModal.css'
-
+import { IoIosCloseCircle } from 'react-icons/io';
 
 const DetailsModal = ({closeModal, section, subsection, title, abstract, url, byline, image}) => {
-	console.log('modal')
 	return (
 		<section className='details-modal'>
+			<img src={image} alt={title}/>
 			<p>{title}</p>
+			<p>{byline}</p>
+			<p>{abstract}</p>
+			<a href={url}>Visit Article Here</a> 
+			<p>{section}</p>
+			<p>{subsection}</p>
+			<IoIosCloseCircle onClick={closeModal}/>
 		</section>
 	)
 }
